4571c2a5a63486f23a99dbb4e9d4030314ee4673
Santiago Herrera <santiagohrd1@gmail.com>
2023-10-14 22:44:21 -0500

    Update dark mode styles

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,14 +1,33 @@
 const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
+const main = document.querySelector("body");
+const eventHero = document.querySelectorAll(".event-hero");
+const weather = document.querySelector(".weather-info");
+const spotCards = document.querySelectorAll('.spot-card');
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🌙")) {
 		main.style.background = "#000";
 		main.style.color = "#fff";
+		eventHero.forEach(function(event){
+			event.style.backgroundColor = "#0B0D22";
+		});
+		weather.style.background = "#0B0D22";
+		spotCards.forEach(function(card) {
+			card.style.backgroundColor = "#0B0D22";
+		});
 		modeButton.textContent = "☀️";
 	} else {
 		main.style.background = "#eee";
 		main.style.color = "#000";
-		modeButton.textContent = "🌙";
+		eventHero.forEach(function(event){
+			event.style.backgroundColor = "#f1f1f1";
+		});
+
+		weather.style.background = "#f1f1f1";
+
+		spotCards.forEach(function(card) {
+			card.style.backgroundColor = "#f1f1f1";
+		});
+		modeButton.textContent = "🌙"; 
 	}
 });
