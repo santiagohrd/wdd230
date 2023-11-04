0ec9c4d8ffe6d0652ff987289926d0579304b663
santiagohrd <santiagohrd1@gmail.com>
2023-11-04 18:07:06 -0500

    Add meta description ans author

--- chamber/scripts/latestVisit.js
+++ chamber/scripts/latestVisit.js
@@ -1,7 +1,20 @@
-const visitsDisplay = document.querySelector(".visits");
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+const divVisit =document.querySelector(".latestVisit");
+const today = Date.now();
+const lastDate = localStorage.getItem("lastVisit");
+let message = "";
 
-visitsDisplay.textContent = numVisits;
+if (!lastDate) {
+    divVisit.textContent = "Welcome! Let us know if you have any questions.";
+} else {
+    const timeDifference = today - parseInt(lastDate);
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-numVisits++;
-localStorage.setItem("numVisits-ls", numVisits);
+    if (daysDifference < 1) {
+        divVisit.textContent = "Back so soon! Awesome!";
+    } else if (daysDifference === 1) {
+        divVisit.textContent = "You last visited 1 day ago.";
+    } else {
+        divVisit.textContent = `You last visited ${daysDifference} days ago.`;
+    }
+}
+localStorage.setItem("lastVisit", today.toString());
