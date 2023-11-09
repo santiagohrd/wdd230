16216c367541ac4842718d458bd3cfbd21b325e6
santiagohrd <santiagohrd1@gmail.com>
2023-11-09 18:25:42 -0500

    Update for cicle in links

--- scripts/links.js
+++ scripts/links.js
@@ -4,23 +4,47 @@ const linksURL = 'https://santiagohrd.github.io/wdd230/data/links.json';
 async function getActivityData(){
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    displayLinks(data.lessons);
 }
 
 getActivityData();
 
-const ul = document.querySelector('ul');
+const ul = document.querySelector('#lessons');
 
-const displayLinks = (weeks) => {
+// function displayLinks(weeks){
 
-    weeks.forEach(lesson => {
+//     weeks.forEach(lesson => {
+//         const li = document.createElement('li');
+//         const a = document.createElement('a');
+
+//         a.setAttribute('href', `${linksURL}`);
+//         a.textContent = `${lesson.lesson}: ${lesson.links[0].title}`;
+
+//         ul.appendChild(li);
+//         li.appendChild(a);
+//     });
+// }
+function displayLinks(weeks) {
+    for (let i = 0; i < weeks.length; i++) {
+        const lesson = weeks[i];
         const li = document.createElement('li');
         const a = document.createElement('a');
 
-        a.href = `${baseURL}${lesson.links.url}`;
-        a.textContent = `${lesson.week}: ${lesson.links.title}`;
+        // Verificar si hay enlaces en la lección actual
+        if (lesson.links.length > 0) {
+            // Acceder a los enlaces dinámicamente según la cantidad disponible
+            for (let j = 0; j < lesson.links.length; j++) {
+                const link = lesson.links[j];
+                const linkText = ` ${lesson.lesson}: ${lesson.links[j].title} |`;
+                const linkURL = `${baseURL}${link.url}`;
 
-        ul.appendChild(li);
-        li.appendChild(a);
-    });
+                const linkElement = document.createElement('a');
+                linkElement.setAttribute('href', linkURL);
+                linkElement.textContent = linkText;
+
+                li.appendChild(linkElement);
+                ul.appendChild(li);
+            }
+        }
+    }
 }
