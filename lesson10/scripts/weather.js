eca60083dab48b764a6d6a3d50426fef94964d15
Santiago Herrera <santiagohrd1@gmail.com>
2023-11-15 05:57:43 -0500

    Add weather files

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,5 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=de1bb7d255158b44de8c8b712ff9e354';
