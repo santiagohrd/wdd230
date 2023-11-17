8ad9b3cbf779343f2c5d33167dd3387d9c0a3ef4
Santiago Herrera <santiagohrd1@gmail.com>
2023-11-17 06:28:39 -0500

    Add url api forcast

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -4,7 +4,10 @@ const captionDesc = document.querySelector('figcaption');
 const humidity = document.querySelector('#humidity');
 const wind = document.querySelector('#wind');
 
+const forcast = document.querySelector('#forcast-temp')
+
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=4.72&lon=-74.1&units=imperial&appid=de1bb7d255158b44de8c8b712ff9e354';
+const urlforcast = 'https://api.openweathermap.org/data/2.5/onecall?lat=4.72&lon=-74.1&exclude=current,minutely,hourly,alerts&appid=de1bb7d255158b44de8c8b712ff9e354'
 
 async function apiFetch(){
     try{
@@ -33,5 +36,9 @@ function displayResults(data){
     captionDesc.textContent = `${desc}`.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));;
 
     humidity.innerHTML = `  ${data.main.humidity}%`;
-    wind.innerHTML = `  ${data.wind.speed}`;
+    wind.innerHTML = `  ${data.wind.speed.toFixed(1)}`;
+
+    forcast.innerHTML = `${urlforcast.daily.slice(1, 4)}`;
+
+
 }
