fe0ca07febee8d02b08d286ba4cd2af96bcabe84
Santiago Herrera <santiagohrd1@gmail.com>
2023-11-17 06:11:26 -0500

    Add the humidity and wind calculation

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,6 +1,8 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+const humidity = document.querySelector('#humidity');
+const wind = document.querySelector('#wind');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=4.72&lon=-74.1&units=imperial&appid=de1bb7d255158b44de8c8b712ff9e354';
 
@@ -23,10 +25,13 @@ apiFetch();
 function displayResults(data){
     currentTemp.innerHTML = `${data.main.temp.toFixed(0)} &deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-
+    
     let desc = data.weather[0].description;
-
+    
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = `  -  ${desc}`.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));;
+    captionDesc.textContent = `${desc}`.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));;
+
+    humidity.innerHTML = `  ${data.main.humidity}%`;
+    wind.innerHTML = `  ${data.wind.speed}`;
 }
