ba278acc3cca9034c93d818ff5d0694ac0c9de7d
Santiago Herrera <santiagohrd1@gmail.com>
2023-12-13 05:50:59 -0500

    Test in data cards

--- scoots/scripts/tablePricing.js
+++ scoots/scripts/tablePricing.js
@@ -1,95 +1,42 @@
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     // Tu JSON con datos de tipo y precio
-//     var jsonFile = 'https://santiagohrd.github.io/wdd230/scoots/data/rentalPricing.json';
-
-//     // Obtener la referencia del tbody
-//     var tablaBody = document.getElementById('tablaBody');
-
-//     // Realizar una solicitud AJAX para cargar el JSON desde el archivo externo
-//     fetch(jsonFile)
-//         .then(response => response.json())
-//         .then(data => {
-//             // Iterar sobre el JSON y agregar filas a la tabla
-//             data.rentalPricing.forEach(function (pricingCategory) {
-//                 pricingCategory.reservation.forEach(function (reservationType) {
-//                     reservationType.type.forEach(function (item) {
-//                         var row = tablaBody.insertRow();
-//                         var cell1 = row.insertCell(0);
-//                         var cell2 = row.insertCell(1);
-//                         var cell3 = row.insertCell(2);
-//                         var cell4 = row.insertCell(3);
-
-//                         cell1.innerHTML = item.name;
-//                         cell2.innerHTML = item.max;
-//                         cell3.innerHTML = '$' + item.halfDay.toFixed(2);
-//                         cell4.innerHTML = '$' + item.fullDay.toFixed(2);
-//                     });
-//                 });
-//             });
-//         })
-//         .catch(error => console.error('Error al cargar el archivo JSON:', error));
-// });
-
-// const data = 'https://santiagohrd.github.io/wdd230/scoots/data/rentalPricing.json';
-
-const data = {
-    "rentalPricing": [
-        {
-            "reservation": [
-                {
-                    "type": [
-                        {
-                            "name": "Honda Metro Scooter",
-                            "max": 1,
-                            "halfDay": 20,
-                            "fullDay": 30                            
-                        },
-                        // ... (otros tipos)
-                    ]
-                }
-            ]
-        },
-        {
-            "walk-in": [
-                {
-                    "type": [
-                        {
-                            "name": "Honda Metro Scooter",
-                            "max": 1,
-                            "halfDay": 25,
-                            "fullDay": 35                            
-                        },
-                        // ... (otros tipos)
-                    ]
-                }
-            ]
-        }
-    ]
-};
 
 
-function generateTable(data) {
-    const tableBody = document.getElementById('tablaBody');
 
-    data.rentalPricing.forEach(pricing => {
-        const category = Object.keys(pricing)[0];
-        const prices = pricing[category][0].type;
+const jsondata = 'https://santiagohrd.github.io/wdd230/scoots/data/rentalPricing.json';
 
-        prices.forEach(price => {
-            const row = document.createElement('tr');
-            row.innerHTML = `
-                <td>${price.name}</td>
-                <td>${price.max}</td>
-                <td>${category === 'reservation' ? price.halfDay : ''}</td>
-                <td>${category === 'reservation' ? price.fullDay : ''}</td>
-                <td>${category === 'walk-in' ? price.halfDay : ''}</td>
-                <td>${category === 'walk-in' ? price.fullDay : ''}</td>
-            `;
-            tableBody.appendChild(row);
-        });
+const metroScooter = document.querySelector("#metroScooter");
+
+async function pricingData(){
+    const response = await fetch(jsondata);
+    const data = await response.json();
+    displayInfo(data.rentalPricing.filter(item => item.type === "Reservation"));
+}
+
+function displayCards(pricing) {
+    const cardsContainer = document.getElementById('cardsContainer');
+
+    pricing.forEach(item => {
+        const card = document.createElement('div');
+        card.classList.add('rental_card');
+        
+        const title = document.createElement('h3');
+        title.textContent = item.name;
+
+        const description = document.createElement('p');
+        description.classList.add('rental_card', 'text');
+        description.textContent = item.description; // Assuming description is present in the JSON
+
+        const image = document.createElement('img');
+        image.src = item.image;
+        image.alt = item.name;
+        image.classList.add('rental_card', 'metro');
+
+        card.appendChild(title);
+        card.appendChild(description);
+        card.appendChild(image);
+
+        cardsContainer.appendChild(card);
     });
 }
 
-// Llamada a la función para generar la tabla
-generateTable(data);
+pricingData(); // Call the function to fetch and display data
